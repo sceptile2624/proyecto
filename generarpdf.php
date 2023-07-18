15b57275520142cdafb49760f9748d9f42872abd
sceptile2624 <sceptile2624@gmail.com>
2023-07-18 11:21:03 -0600

    l

--- generarpdf.php
+++ generarpdf.php
@@ -6,93 +6,52 @@ include "conexion.php";
 include "fpdf185/fpdf.php";
 require "vendor/autoload.php";
 
+
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 $user = $_GET["user"];
 
-// USUARIO
-$idUsuario;
-$nombre;
-$contrasena;
-$correo;
-$direccion;
-$cp;
+require 'vendor/autoload.php';
 
-$total = 0;
-
-$usuario = $conexion->query("SELECT * FROM usuarios WHERE correo = '$user'");
-
-if ($usuario && $usuario->num_rows > 0) {
-    $fila = $usuario->fetch_assoc();
-    $idUsuario = $fila['id'];
-    $nombre = $fila['nombre'];
-    $correo = $fila['correo'];
-    $contrasena = $fila['contrasena'];
-}
-
-// CARRITO
-$resultado_carrito = $conexion->query("SELECT * FROM carrito");
-
-// Crear un nuevo objeto FPDF
+$mail = new PHPMailer(true);
 $pdf = new FPDF();
-// Agregar una nueva página al PDF
 $pdf->AddPage();
-
-// Generar el contenido del PDF
-$pdf->SetFont('Arial', 'B', 18);
-$pdf->Cell(0, 10, 'Este mensaje salió de Le cortamos los pelos', 0, 1);
-$pdf->Ln(10);
-$pdf->SetFont('Arial', 'B', 14);
-$pdf->Cell(0, 10, 'Para: ' . $nombre, 0, 1);
-$pdf->Cell(0, 10, 'Productos:', 0, 1);
-
-if ($resultado_carrito && $resultado_carrito->num_rows > 0) {
-    while ($fila_carrito = $resultado_carrito->fetch_assoc()) {
-        // Obtener los datos específicos del carrito
-        $nombre_producto = $fila_carrito['nombre_producto'];
-        // Otros campos del carrito
-        $precio = $fila_carrito['precio'];
-        $total += $precio;
-
-        // Agregar los datos del carrito al PDF
-        $pdf->Cell(0, 10, "\t\t$nombre_producto  $$precio", 0, 1);
-        // Agregar otros campos del carrito al PDF
-
-    }
-    $pdf->Cell(0, 10, "\t\tTotal: $total", 0, 1);
+$pdf->SetFont('Arial', 'B', 16);
+while ($row = $sql->fetch_assoc()) {
+    $pdf->MultiCell(160, 10, strval('Nombre Producto: ' . $row['nombre_producto'] . '   ' . 'Precio: $ ' . $row['precio']), 1, 'L');
 }
-$fecha = date('l jS \of F Y h:i:s A');
-$pdf->Cell(0, 10, 'Fecha: ' . $fecha, 0, 1);
+$pdfdoc = $pdf->Output("Doc", "S");
+$pdflisto = chunk_split(base64_encode($pdfdoc));
 
-// Guardar el PDF en el servidor
-$pdfPath = 'pdf/orden' . $idUsuario . '.pdf';
-$pdf->Output($pdfPath, 'F');
+try {
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'a21310355@ceti.mx';
+    $mail->Password = '00Camila';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-// Definir los encabezados del correo electrónico
-$mail = new PHPMailer();
-$mail->CharSet = 'utf-8';
-$mail->Host = "smtp.googlemail.com";
-$mail->From = "sceptile2624@gmail.com";
-$mail->IsSMTP();
-$mail->SMTPAuth = true;
-$mail->Username = "sceptile2624@gmail.com";
-$mail->Password = "hxlnpvqlmvxfczrs";
-$mail->SMTPSecure = "ssl";
-$mail->Port = 465;
-$mail->AddAddress($correo);
-$mail->SMTPDebug = 0;
-$mail->isHTML(true);
-$mail->Subject = 'GRACIAS POR TU PREFERENCIA!';
-$mail->Body = '<b>Adjuntamos un resumen de tu compra</b>';
-$mail->AltBody = 'Te mandamos el resumen de tu compra';
+    $mail->setFrom('a21310355@ceti.mx', 'Alejandro Marquez');
+    $mail->addAddress('marquez1alejandro@gmail.com', 'Receptor');
+    $mail->addCC('a21310355@ceti.mx');
 
-$inMailFileName = "recibo.pdf";
-$filePath = 'pdf/orden' . $idUsuario . '.pdf';
-$mail->AddAttachment($filePath, $inMailFileName);
+    $mail->addStringAttachment($pdfdoc, 'Doc.pdf');
 
-if ($mail->send()) {
-    header('Location: ../EliminarCarrito.php?user=' . $user);
-    exit();
-} else {
-    echo "Error al enviar el correo: " . $mail->ErrorInfo;
-    exit();
+    $mail->isHTML(true);
+    $mail->Subject = 'Prueba desde GMAIL';
+    $mail->Body = 'Hola, <br/>Esta es una prueba desde <b>Gmail</b>.';
+    $mail->send();
+
+    echo 'Correo enviado';
+} catch (Exception $e) {
+    echo 'Mensaje ' . $mail->ErrorInfo;
 }
+$sql = mysqli_query($conexion, "INSERT INTO detalles SELECT 0, nombre_producto, CURDATE(), precio FROM carrito");
+$vaciar = mysqli_query($conexion, "TRUNCATE TABLE carrito");
+header("Location: ./html_usuarios/Productos_usuarios.php");
 ?>
