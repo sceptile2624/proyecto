f2536d16363e3493447b0942ff4c0991f6cf9792
sceptile2624 <sceptile2624@gmail.com>
2023-07-18 10:17:13 -0600

    g

--- html_admin/agregarcarrito.php
+++ html_admin/agregarcarrito.php
@@ -23,6 +23,8 @@ if ($userSelect->num_rows > 0) {
 $insert = $conexion->query("INSERT INTO carrito (id_producto, nombre_producto, url_img, precio, nombre_usuario)
                             VALUES ('$id', '$titulo', '$url_img', '$precio', '$nombreUsuario')");
 if ($insert) {
-    header("location: ../html_usuarios/productos_usuarios.php?user=$user");
+    echo "El registro se ha agregado correctamente al carrito.";
+} else {
+    echo "Error al agregar el registro al carrito: " . $conexion->error;
 }
 ?>
