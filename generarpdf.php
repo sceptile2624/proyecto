d7f4575821960bc1d23803322bcc4e1f6d283648
sceptile2624 <sceptile2624@gmail.com>
2023-07-19 10:41:07 -0600

    o

--- generarpdf.php
+++ generarpdf.php
@@ -3,55 +3,107 @@ session_start();
 
 use PHPMailer\PHPMailer\PHPMailer;
 include "conexion.php";
-include "../proyecto/fpdf185/fpdf.php";
+include "fpdf185/fpdf.php";
 require "vendor/autoload.php";
-
-
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
 
 $user = $_GET["user"];
 
-require 'vendor/autoload.php';
+//USUARIO
+$idUsuario;
+$nombre;
+$contrasena;
+$correo;
+$direccion;
+$cp;
 
-$mail = new PHPMailer(true);
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 16);
-while ($row = $sql->fetch_assoc()) {
-    $pdf->MultiCell(160, 10, strval('Nombre Producto: ' . $row['nombre_producto'] . '   ' . 'Precio: $ ' . $row['precio']), 1, 'L');
+$total = 0;
+
+$usuario = $conexion -> query("SELECT * FROM usuarios WHERE correo = '$user'");
+
+if($usuario && $usuario->num_rows>0){
+    $fila = $usuario->fetch_assoc();
+    $idUsuario = $fila['id'];
+    $nombre = $fila['nombre'];
+    $correo = $fila['correo'];
+    $contrasena = $fila['contrasena'];    
 }
-$pdfdoc = $pdf->Output("Doc", "S");
-$pdflisto = chunk_split(base64_encode($pdfdoc));
 
-try {
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'a21310355@ceti.mx';
-    $mail->Password = '00Camila';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+//CARRITO
+$resultado_carrito = $conexion->query("SELECT * FROM carrito");
 
-    $mail->setFrom('a21310355@ceti.mx', 'Alejandro Marquez');
-    $mail->addAddress('marquez1alejandro@gmail.com', 'Receptor');
-    $mail->addCC('a21310355@ceti.mx');
+    // Crear un nuevo objeto FPDF
+    $pdf = new FPDF();
+    // Agregar una nueva página al PDF
+    $pdf->AddPage();
 
-    $mail->addStringAttachment($pdfdoc, 'Doc.pdf');
+    // Generar el contenido del PDF
+    $pdf->SetFont('Arial', 'B', 18);
+    $pdf->Cell(0, 10, 'Este mensaja salio de Le cortamos los pelos', 0, 1);
+    $pdf->Ln(10); // Cambio de ln a Ln
+    $pdf->SetFont('Arial', 'B', 14);
+    $pdf->Cell(0, 10, 'Para: '.$nombre, 0, 1);
+    $pdf->Cell(0, 10, 'Productos:', 0, 1);
 
-    $mail->isHTML(true);
-    $mail->Subject = 'Prueba desde GMAIL';
-    $mail->Body = 'Hola, <br/>Esta es una prueba desde <b>Gmail</b>.';
-    $mail->send();
+    if ($resultado_carrito && $resultado_carrito->num_rows > 0) {
+        while ($fila_carrito = mysqli_fetch_assoc($resultado_carrito)) {
+            // Obtener los datos específicos del carrito
+            $nombre_producto = $fila_carrito['nombre_producto'];
+            // Otros campos del carrito
+            $precio = $fila_carrito['precio'];
+            $total += $precio;
 
-    echo 'Correo enviado';
-} catch (Exception $e) {
-    echo 'Mensaje ' . $mail->ErrorInfo;
+            // Agregar los datos del carrito al PDF
+            $pdf->Cell(0, 10, "\t\t$nombre_producto  $$precio", 0, 1);
+            // Agregar otros campos del carrito al PDF
+            
+        }
+        $pdf->Cell(0, 10, "\t\tTotal: $total", 0, 1);
+    }
+    $fecha = date('l jS \of F Y h:i:s A');
+    $pdf->Cell(0, 10, 'Fecha: ' . $fecha, 0, 1); // Cambio de $datos_historial['fecha'] a $fecha
+
+      // Guardar el PDF en el servidor
+    $pdfPath = 'pdf/orden'.$idUsuario.'.pdf';
+    $pdf->Output($pdfPath, 'F');
+
+    // Definir los encabezados del correo electrónico
+    $mail = new PHPMailer();
+	$mail->CharSet = 'utf-8';
+	$mail->Host = "smtp.googlemail.com";
+	$mail->From = "sceptile2624@gmail.com";
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true;
+	$mail->Username = "sceptile2624@gmail.com";
+	$mail->Password = "hxlnpvqlmvxfczrs";
+	$mail->SMTPSecure = "ssl";
+	$mail->Port = 465;
+	$mail->AddAddress($correo);
+	$mail->SMTPDebug = 0;   //Muestra las trazas del mail, 0 para ocultarla
+	$mail->isHTML(true);                                  // Set email format to HTML
+	$mail->Subject = 'GRACIAS POR TU PREFERENCIA!';
+	$mail->Body = '<b>Adjuntamos un resumen de tu compra</b>';
+	$mail->AltBody = 'Te mandamos el resumen de tu compra';
+
+	$inMailFileName = "recibo.pdf";
+	$filePath = 'pdf/orden'.$idUsuario.'.pdf';
+	$mail->AddAttachment($filePath, $inMailFileName);
+
+	$mail->send();
+    
+    header('location: EliminarCarrito.php?user='.$user);
+
+    $pdf = new FPDF();
+// Resto del código...
+
+if ($pdf->Output($pdfPath, 'F')) {
+    // PDF generado correctamente
+    // Resto del código de envío de correo
+    // ...
+    // Envío de correo electrónico con el PDF adjunto
+    // ...
+    header('location: EliminarCarrito.php?user='.$user);
+} else {
+    // Error al generar el PDF
+    echo "Hubo un error al generar el PDF.";
 }
-$sql = mysqli_query($conexion, "INSERT INTO detalles SELECT 0, nombre_producto, precio, id_usuario FROM carrito");
-$vaciar = mysqli_query($conexion, "TRUNCATE TABLE carrito");
-header("Location: ../html_usuarios/Productos_usuarios.php");
 ?>
