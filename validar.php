b53bba161cfe46900febaba1ed0691397b578402
sceptile2624 <sceptile2624@gmail.com>
2023-07-19 11:54:04 -0600

    w

--- validar.php
+++ validar.php
@@ -1,29 +1,23 @@
 <?php
 include('conexion.php');
-$correo=$_POST['correo'];
-$contrasena=$_POST['contrasena'];
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
 
-
-
-$consulta= "SELECT id, nombre, contrasena, correo, direccion, cp FROM usuarios";
-$resultado=mysqli_query($conexion,$consulta);
+$consulta = "SELECT id, nombre, contrasena, correo, direccion, cp FROM usuarios WHERE correo = '$correo' AND contrasena = '$contrasena'";
+$resultado = mysqli_query($conexion, $consulta);
 
 $entrar = mysqli_num_rows($resultado);
 
-
-if($entrar > 0){
-  
-    header(" html_usuarios/productos_usuarios.php?user=$correo");
+if ($entrar > 0) {
+    // Autenticación exitosa, redireccionar a la página de productos con el correo como parámetro en la URL
+    header("Location: html_usuarios/productos_usuarios.php?user=$correo");
     exit;
-
-}else{
-    ?>
-    <?php
-    include("index.html");
-
-  ?>
-  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
-  <?php
+} else {
+    // Autenticación fallida, redireccionar al formulario de inicio de sesión nuevamente
+    header("Location: index.html");
+    exit;
 }
+
 mysqli_free_result($resultado);
 mysqli_close($conexion);
+?>
