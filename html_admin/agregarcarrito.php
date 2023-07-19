a47a9d1af1ffadafaf00b5e49572bb55109efa7e
sceptile2624 <sceptile2624@gmail.com>
2023-07-19 12:15:28 -0600

    c1

--- html_admin/agregarcarrito.php
+++ html_admin/agregarcarrito.php
@@ -1,8 +1,22 @@
 <?php
+session_start(); // Iniciar la sesión para utilizar las variables de sesión
+
 include 'conexion.php';
 
 $a = $_GET["var"];
 $user = $_GET["user"];
+
+// Obtener el nombre del usuario desde la tabla usuarios
+$userSelect = $conexion->query("SELECT nombre_usuario FROM usuarios WHERE correo = '$user'");
+if ($userSelect->num_rows > 0) {
+    $userRow = $userSelect->fetch_assoc();
+    $nombreUsuario = $userRow["nombre_usuario"];
+} else {
+    // Manejar la situación si el usuario no existe
+    // Aquí puedes redireccionar a una página de error o realizar otras acciones
+    header("Location: ../proyecto/html_usuarios/productos_usuarios.php?user=$user");
+    exit;
+}
 
 $select = $conexion->query("SELECT id, producto, precio, url_img FROM productos WHERE id = $a");
 if ($select->num_rows > 0) {
@@ -11,18 +25,23 @@ if ($select->num_rows > 0) {
     $titulo = $row["producto"];
     $precio = $row["precio"];
     $url_img = $row["url_img"];
-}
 
-// Obtener el nombre del usuario desde la tabla usuarios
-$userSelect = $conexion->query("SELECT nombre_usuario FROM usuarios WHERE correo = '$user'");
-if ($userSelect->num_rows > 0) {
-    $userRow = $userSelect->fetch_assoc();
-    $nombreUsuario = $userRow["nombre_usuario"];
-}
+    // Agregar el producto al carrito usando la matriz de sesión
+    // Comprobar si la matriz de sesión de carrito ya existe, si no, crearla.
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = array();
+    }
 
-$insert = $conexion->query("INSERT INTO carrito (id_producto, nombre_producto, url_img, precio, nombre_usuario)
-                            VALUES ('$id', '$titulo', '$url_img', '$precio', '$nombreUsuario')");
-if ($insert) {
+    // Agregar el producto a la matriz de sesión del carrito.
+    // Aquí, estamos usando el id del producto como clave y el correo electrónico del usuario como valor.
+    $_SESSION['cart'][$id] = $user;
+
+    // Redireccionar a la página de productos del usuario.
+    header("Location: ../proyecto/html_usuarios/productos_usuarios.php?user=" . urlencode($user));
+    exit;
+} else {
+    // Manejar la situación si el producto no existe
+    // Aquí puedes redireccionar a una página de error o realizar otras acciones
     header("Location: ../proyecto/html_usuarios/productos_usuarios.php?user=$user");
     exit;
 }
